fc640e04c5ca819c80f1b9b09b29c7881eaba71c
Jonathan Araujo <jonathan.araujo76@icloud.com>
2023-06-12 14:32:05 -0300

    Iniciando o projeto.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <aside>
+    <BarraLateral />
+  </aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Olá, Mundo</h1>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    
+    export default defineComponent({
+        name: 'BarraLateral'
+    })
+</script>
